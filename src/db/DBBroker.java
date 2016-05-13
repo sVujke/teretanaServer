e8fe9cc5e4e23b193c72c74810330be0d6c56e5d
vujke <stefanvujovic93@gmail.com>
2016-05-13 15:01:07 +0200

    util dodat

--- src/db/DBBroker.java
+++ src/db/DBBroker.java
@@ -5,10 +5,49 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author vujke
  */
 public class DBBroker {
+    private Connection konekcija;
+    private static DBBroker instance;
+
+    public DBBroker() {
+    }
     
+    public static DBBroker vratiInstancu(){
+        if(instance==null){
+            instance = new DBBroker();
+        }
+        
+        return instance;
+    }
+    
+    public void konektujSe(){
+    
+    }
+    
+    public void diskonektujSe(){
+    
+    }
+    
+    public void potvrdiTransakciju() {
+        try {
+            konekcija.commit();
+        } catch (SQLException ex) {
+            System.out.println("Neuspesan commit.");
+        }
+    }
+    
+    public void ponistiTransakciju() {
+        try {
+            konekcija.rollback();
+        } catch (SQLException ex) {
+            System.out.println("Neuspesan rollback.");
+        }
+    }
 }
---
+++ src/util/Konstante.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author vujke
+ */
+public class Konstante {
+    
+    // db connection url 
+    public static final String url = "jdbc:mysql://localhost:3306/IME_BAZE";
+    public static final String user = "root";
+    public static final String pass = "";
+}
